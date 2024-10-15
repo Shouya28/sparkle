a61d46d02eda3a0e264dc095fa9ce395c7b93192
Ryzxell <161612079+Shouya28@users.noreply.github.com>
2024-10-15 22:48:12 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Benefits from "@/components/Benefits";
 import Pricing from "@/components/Pricing";
 import FAQSection from "@/components/FrequentlyAsked";
 import Footer from "@/components/Footer";
+import AutoplayAudio from '../components/AutoplayAudio';
 
 export default function Home() {
 	return (
